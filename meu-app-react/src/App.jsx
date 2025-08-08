7821cbe0eec98ef32b58c3f97ec6a17c2acb3a1a
Eliana Ritzmann <eliana.cristinas2@hotmail.com>
2025-08-08 00:21:31 -0300

    ex2 3 4 e 5(precisa consertar a data)

--- meu-app-react/src/App.jsx
+++ meu-app-react/src/App.jsx
@@ -6,9 +6,21 @@ function App() {
 
   const [titulo,setTitulo] = useState('')
   const [descricao, setDescricao] = useState('')
+  const[url, setUrl] =useState('')
+  const[data, setData]= useState('')
+  const[categoria, setCategoria]= useState('')
+  const hoje = new Date()
    function save(event){
     event.preventDefault()
-
+    if(titulo === '' || descricao === ''){
+      alert('Esses campos não podem ficar vazios')
+    }else if(!url.startsWith('http')){
+      alert('link invalido')
+    }else if(categoria === ''){
+      alert('selecione uma categoria')
+    }else if (data<hoje){
+      alert('data invalida')
+    }
    }
   return(
     <>
@@ -16,14 +28,23 @@ function App() {
         <h1>Painel de gerenciamento</h1>
         <span>Novo post</span>
        <form action="" onSubmit={save}>
-        <input type="text" placeholder='Titulo' value={titulo}
+        <label htmlFor="titulo">Titulo</label>
+        <input type="text" name='titulo' placeholder='Titulo' value={titulo}
         onChange={(event)=>{ setTitulo(event.target.value)}} />
-
+        <label htmlFor="descricao">Descrição</label>
         <textarea name="descricao" id="descricao" placeholder='descrição' value={descricao} 
         onChange={(event)=>{ setDescricao(event.target.value)}
         }>
-
         </textarea>
+        <input type="text" placeholder='URL da imagem de capa'value={url} onChange={(event)=>{ setUrl(event.target.value)} } />
+        <input type="date" placeholder='Data de publicação' value={data} onChange={(event)=>{ setData(event.target.value)} } />
+        <label htmlFor="categoria">Tipo do post: </label>
+        <select name="categoria" id="categoria" value={categoria} onChange={(event)=>{ setCategoria(event.target.value)} } >
+          <option value="artigo">Artigo</option>
+          <option value="noticia">Notícia</option>
+          <option value="tutorial">Tutorial</option>
+          <option value="entrevista">Entrevista</option>
+        </select>
         <button type='submit'>Publicar</button>
        </form>
       </div>
